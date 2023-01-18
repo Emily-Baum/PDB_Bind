f3204990bf7b384f0522b5c81655e7c658e6a841
danielgarzonotero <vvd9fd@virginia.edu>
2023-01-18 16:57:49 -0500

    second version

--- PDB_Bind.py
+++ PDB_Bind.py
@@ -7,62 +7,69 @@ import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.data import Dataset, DataLoader
 
-
-class ConvNet(nn.Module): #Las clases deben empezar con MAYUS, la clase padre es nn.Modulo y la clase hijo es ConvNet
+#-------------- Model ----------------:
+class ConvNet(nn.Module): 
     
-    def __init__(self): #defines the core architecture of the model that is, all the layers with the number of neurons at each layer. CONSTRUCTOR
+    def __init__(self): 
         super(ConvNet, self).__init__() 
         input_size = 2048
-        hidden_size1 = 20
-        hidden_size2 = 10
-        hidden_size3 = 3
+        hidden_size1 = 45
+        hidden_size2 = 20
+        hidden_size3 = 7
         output_size = 1
 
         self.fl1 = nn.Linear(input_size, hidden_size1) 
         self.fl2 = nn.Linear(hidden_size1, hidden_size2) 
         self.fl3 = nn.Linear(hidden_size2, hidden_size3) 
         self.fl4 = nn.Linear(hidden_size3, output_size) 
-    def forward(self, x): #the forward function does a forward pass in the network. It includes all the activation functions at each layer. ACCIONES
+        
+    def forward(self, x):
         out = self.fl1(x)
-        out = F.relu(out) #activation function
+        out = F.relu(out) 
         out = self.fl2(out)
-        out = F.relu(out) #activation function
+        out = F.relu(out) 
         out = self.fl3(out)
-        out = F.relu(out) #activation function
+        out = F.relu(out) 
         out = self.fl4(out)
         
         return out 
 
-def train(model, device, train_dataloader, optim, epoch): #the actual backpropagation step
+#-------------- Train routine ----------------:
+def train(model, device, train_dataloader, optim, epoch): 
     model.train()
 
     loss_func = torch.nn.L1Loss(reduction='sum') 
     for b_i, (fp, y) in enumerate(train_dataloader):
 
         fp, y = fp.to(device), y.to(device)
-        optim.zero_grad() #despues de la optimizacion hay que reiniciar el gradiente, esto lo hace esta funcion
+
+        optim.zero_grad() 
         pred_prob = model(fp.float())
+
         loss = loss_func(pred_prob, y)
-        loss.backward() #calcula la perdida con respecto a la entrada, backward propagation
-        optim.step() #optimization step, update weights
+        loss.backward() 
+
+        optim.step() 
         
         if b_i % 10 == 0: #"Cada 10 pasos"
             print('epoch: {} [{}/{} ({:.0f}%)]\t training loss: {:.6f}'.format(
                 epoch, b_i * len(fp), len(train_dataloader.dataset),
                 100. * b_i / len(train_dataloader), loss.item()))
 
-def test(model, device, test_dataloader): #Para evaluar el desempeño de la NN
+#-------------- Validation Routine ----------------:
+
+def test(model, device, test_dataloader): 
 
     model.eval()
-    loss = 0 #inicialización de la perdida
+    loss = 0 
     loss_func = torch.nn.L1Loss(reduction='sum') 
 
-    with torch.no_grad(): #El with torch.no_grad() es para hacer operaciones SIN la gradient function
+    with torch.no_grad(): 
         for fp, y in test_dataloader:
             fp, y = fp.to(device), y.to(device) 
             pred_prob = model(fp)
-            loss += loss_func(pred_prob, y).item()  # loss summed across the batch
-            pred = pred_prob.argmax(dim=1, keepdim=True)  # us argmax to get the most likely prediction
+            loss += loss_func(pred_prob, y).item()  
+            pred = pred_prob.argmax(dim=1, keepdim=True) 
             
 
     loss /= len(test_dataloader.dataset)
@@ -70,33 +77,31 @@ def test(model, device, test_dataloader): #Para evaluar el desempeño de la NN
     print('\nTest dataset: Overall Loss: {:.4f},  ({:.0f}%)\n'.format(
         loss, len(test_dataloader.dataset)))
 
+#-------------- Dataset ----------------:
 class PDB(Dataset):
-    def __init__(self, fp, y):
-        self.fp = fp
-        self.y = y
+    def __init__(self,path):
+
+        self.df = pd.read_csv(path)
+
+        self.fp= self.df[self.df.columns[0:-1]].values
+        
+        self.y = self.df[self.df.columns[-1]].values
 
     def __len__(self):
         return len(self.y)
 
-    def __getitem__(self, idx): #idx means index
-        fp = self.fp[idx]
-        y = self.y[idx]
+    def __getitem__(self, index): 
+        fp = self.fp[index]
+        y = self.y[index]
         
-        return fp, y
+        return torch.tensor(fp, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)
 
-df = pd.read_csv('pdbind_full_fp2.csv')
+data_set = PDB('pdbind_full_fp2.csv') 
 
-
-fp = torch.tensor(np.array(df[df.columns[0:-1]])).type(torch.float32)
-y = torch.tensor(np.array(df['-logKd/Ki'])).type(torch.float32)
-
-#DataSet
-data_set = PDB(fp, y) #llama la clase PDB
-
+#-------------- Data Loader ----------------:
 batch_size = 3
 
-#DataLoader
-data_loader = DataLoader(data_set, batch_size=batch_size,shuffle=True ) #With collate_fn then the output are tensors. el tamaño del lote se establece en 2. Esto significa que cuando recorre el conjunto de datos, DataLoader generará 2 instancias de datos en lugar de una
+data_loader = DataLoader(data_set, batch_size=batch_size,shuffle=True ) 
 
 train_loader = torch.utils.data.DataLoader(dataset = data_set, 
                                            batch_size = batch_size, 
@@ -106,13 +111,14 @@ test_loader = torch.utils.data.DataLoader(dataset = data_set,
                                           batch_size = batch_size, 
                                           shuffle=True)
 
+#-------------- Run training loop ----------------:
 learning_rate = 0.5
 torch.manual_seed(0)
 device = torch.device("cpu")
 
-model = ConvNet() #Model es el objeto que tiene la clase ConvNet
-optimizer = optim.Adadelta(model.parameters(), lr = learning_rate) #lr = learning rate , model.parameters llama a los parametros del modelo 
+model = ConvNet() 
+optimizer = optim.Adadelta(model.parameters(), lr = learning_rate) 
 
-for epoch in range(1, 3): #For only two epochs (1 an 2) , training loop 
+for epoch in range(1, 3): 
     train(model, device, train_loader, optimizer, epoch)
     test(model, device, test_loader)
